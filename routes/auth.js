9fe31af7c83007678f778414d9b4445c724185da
RafaelCV <cavr134@gmail.com>
2021-06-30 14:19:54 -0500

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+/*
+    RUTAS DE Usuarios /auth
+    host +  /api/auth
+*/
+
+const { Router } = require('express');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { check } = require('express-validator');
+const router = Router();
+
+router.post(
+    '/new',
+    [ //middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ],
+    crearUsuario
+);
+
+router.post(
+    '/',
+    [ //middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ],
+    loginUsuario
+);
+
+router.get('/renew', revalidarToken);
+
+
+module.exports = router;
