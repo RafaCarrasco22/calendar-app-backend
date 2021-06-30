3d51dce56af851652297f7883a91e40b93e267a1
RafaelCV <cavr134@gmail.com>
2021-06-30 17:41:15 -0500

    mongo added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,39 +1,35 @@
 const { response } = require('express');
 const { validationResult } = require('express-validator');
+const Usuario = require('../models/Usuario');
 
+const crearUsuario = async (req, res = response) => {
+    const { email, password } = req.body;
+    try {
+        let usuario = Usuario.findOne(email);
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Un usuario existe con ese correo'
+            });
+        }
+        usuario = new Usuario(req.body);
+        await usuario.save();
 
-const crearUsuario = (req, res = response) => {
-    const { name, email, password } = req.body;
-
-    //manejo de errores
-    const errors = validationResult(req);
-    if( !errors.isEmpty() ) {
-        return res.status(400).json({
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name
+        });
+    } catch (error) {
+        res.status(500).json({
             ok: false,
-            errors: errors.mapped()
-        })
+            msg: 'F x el registrado'
+        });
     }
-
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    })
 }
 
 const loginUsuario = (req, res = response) => {
     const { name, email, password } = req.body;
-    
-    //manejo de errores
-    const errors = validationResult(req);
-    if( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
 
     res.status(201).json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@
 const { Router } = require('express');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
 router.post(
@@ -13,7 +14,8 @@ router.post(
     [ //middlewares
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
     ],
     crearUsuario
 );
@@ -22,7 +24,8 @@ router.post(
     '/',
     [ //middlewares
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
     ],
     loginUsuario
 );
